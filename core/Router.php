eb079b64cb94d446a4c5cdf5a7c5e1051c9bff75
Ярослав <Yaroslav@MacBook-Air-Cybermart.local>
2025-12-22 04:11:14 +0500

    Исправить обработку пустого URI в Router

--- core/Router.php
+++ core/Router.php
@@ -38,6 +38,11 @@ class Router {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+        // Если URI пустой, устанавливаем корень
+        if (!$uri) {
+            $uri = '/';
+        }
+
         // Удаляем trailing slash
         $uri = rtrim($uri, '/');
 
